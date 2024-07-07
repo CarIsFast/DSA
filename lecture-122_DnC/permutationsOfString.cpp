c34004243c47ed33824b31d22adb46674ad749dd
CarIsFast <chinmay1raskar@gmail.com>
2024-07-07 15:17:46 +0530

    dnc 2

--- lecture-122_DnC/permutationsOfString.cpp
+++ lecture-122_DnC/permutationsOfString.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-
+void solve(string &s, int i){
+    //base case
+    if(i>=s.length()){
+        cout<<s<<" ";
+        return;
+    }
+    //firet solution
+    for (int j=i; j<s.length(); j++){
+        swap(s[i],s[j]);
+        solve(s,i+1);
+        //back tracking
+        swap(s[i],s[j]);
+    }
+}
 int main() {
-    
+    string s = "abc";
+    solve(s,0);
     return 0;
 }
