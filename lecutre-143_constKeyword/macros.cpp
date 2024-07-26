3097fd0154954af4bede6806dcb841e1c950dce7
CarIsFast <chinmay1raskar@gmail.com>
2024-07-27 01:04:23 +0530

    macros

---
+++ lecutre-143_constKeyword/macros.cpp
@@ -0,0 +1,7 @@
+#define pi 3.2456
+// macros can also be function 
+
+#define max(x,y) {x > y ? x : y} //function to find max 
+//syntax tu define a macro
+//easy to make changes in const value used at many places
+//does not take any space
